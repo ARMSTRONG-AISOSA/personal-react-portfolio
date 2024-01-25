18d632658becadfe7c16540150375ba6e0a31cfc
Armstrong Omoregie <ammycan6@gmail.com>
2024-01-24 20:21:00 -0800

    3rd - Continue

---
+++ src/components/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+  return (
+    <div>
+      Contact
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <Header>
+      <h1>Hello</h1>
+    </Header>
+  )
+}
+
+export default Header;
